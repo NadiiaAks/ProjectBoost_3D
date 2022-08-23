bd193de2a3ab56b4d7f3784c52514621b2c56856
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-08-23 21:07:40 +0300

    Add audio sourse

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,10 +7,12 @@ public class Movement : MonoBehaviour
     [SerializeField] float mainThrust = 0f;
     [SerializeField] float rotateSpeed = 0f;
     Rigidbody rb;
+    AudioSource audioSource;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
     private void Update()
     {
@@ -23,6 +25,14 @@ public class Movement : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
         }
     }
 
@@ -41,6 +51,8 @@ public class Movement : MonoBehaviour
 
     void ApplyRotation(float rotationThisFrame)
     {
+        rb.freezeRotation = true;
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; 
     }
 }
