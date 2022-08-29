aca6e5d3366612cbe75d99268dfe798d81da50ba
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-08-29 16:20:14 +0300

    Add particle system for thrusting and rotation Refactoring Thrusting and rotation methods

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,6 +7,9 @@ public class Movement : MonoBehaviour
     [SerializeField] float mainThrust = 0f;
     [SerializeField] float rotateSpeed = 0f;
     [SerializeField] AudioClip mainEngine;
+    [SerializeField] ParticleSystem thrustEffect;
+    [SerializeField] ParticleSystem leftRotationEffect;
+    [SerializeField] ParticleSystem rightRotationEffect;
 
     Rigidbody rb;
     AudioSource audioSource;
@@ -27,29 +30,70 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.PlayOneShot(mainEngine);
-            }
+            StartThrusting();
         }
         else
         {
-            audioSource.Stop();
+            StopThrusting();
         }
     }
-
 
     void ProcessRotation()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            ApplyRotation(rotateSpeed);
+            RotateLeft();
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            ApplyRotation(-rotateSpeed);
+            RotateRight();
         }
+        else
+        {
+            StopRotation();
+        }
+    }
+
+    private void StartThrusting()
+    {
+        rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        if (!thrustEffect.isPlaying)
+        {
+            thrustEffect.Play();
+        }
+    }
+    private void StopThrusting()
+    {
+        audioSource.Stop();
+        thrustEffect.Stop();
+    }
+
+    private void RotateRight()
+    {
+        ApplyRotation(-rotateSpeed);
+        if (!leftRotationEffect.isPlaying)
+        {
+            leftRotationEffect.Play();
+        }
+    }
+
+    private void RotateLeft()
+    {
+        ApplyRotation(rotateSpeed);
+        if (!rightRotationEffect.isPlaying)
+        {
+            rightRotationEffect.Play();
+        }
+    }
+
+    private void StopRotation()
+    {
+        rightRotationEffect.Stop();
+        leftRotationEffect.Stop();
     }
 
     void ApplyRotation(float rotationThisFrame)
